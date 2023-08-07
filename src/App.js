5b54fbd8a68cfedaf1ac7728a4d42dcb587d004b
thanhminhspk05 <69253365+thanhminhspk05@users.noreply.github.com>
2023-08-07 17:15:50 +0700

    add currency

--- src/App.js
+++ src/App.js
@@ -4,20 +4,69 @@ import './App.css';
 
 function App() {
   const [data, setData] = useState([
-    { name: 'Minh', email: 'thanhminh@gmail.com', phone: '0945036531', age: 22, gender: 'M', city: 'Phu Yen' },
-    { name: 'Nam', email: 'trungnam@gmail.com', phone: '0945036532', age: 23, gender: 'M', city: 'Ho Chi Minh' },
-    { name: 'Thien', email: 'dongthien@gmail.com', phone: '0945036533', age: 24, gender: 'M', city: 'Phu Yen' },
-    { name: 'My', email: 'hanhmy@gmail.com', phone: '0945036534', age: 25, gender: 'F', city: 'Phu Yen' },
-    { name: 'Van', email: 'hongvan@gmail.com', phone: '0945036535', age: 25, gender: 'F', city: 'Phu Yen' },
+    {
+      name: 'Minh',
+      email: 'thanhminh@gmail.com',
+      phone: '0945036531',
+      age: null,
+      gender: 'M',
+      city: 'Phu Yen',
+      fee: 12342,
+    },
+    {
+      name: 'Nam',
+      email: 'trungnam@gmail.com',
+      phone: '0945036532',
+      age: 23,
+      gender: 'M',
+      city: 'Ho Chi Minh',
+      fee: 22342,
+    },
+    {
+      name: 'Thien',
+      email: 'dongthien@gmail.com',
+      phone: '0945036533',
+      age: 24,
+      gender: 'M',
+      city: 'Phu Yen',
+      fee: 32342,
+    },
+    {
+      name: 'My',
+      email: 'hanhmy.nguyenba@gmail.com',
+      phone: '0945036534',
+      age: 25,
+      gender: 'F',
+      city: 'Phu Yen',
+      fee: 32342,
+    },
+    {
+      name: 'Van',
+      email: 'hongvan.nguyen@gmail.com',
+      phone: '0945036535',
+      age: 25,
+      gender: 'F',
+      city: 'Phu Yen',
+      fee: 62342,
+    },
   ]);
 
   const columns = [
     { title: 'Name', field: 'name' },
     { title: 'Email', field: 'email' },
     { title: 'Phone Number', field: 'phone', align: 'right' },
-    { title: 'Age', field: 'age' },
-    { title: 'Gender', field: 'gender' },
+    {
+      title: 'Age',
+      field: 'age',
+      emptyValue: () => (
+        <>
+          <em>null</em>
+        </>
+      ),
+    },
+    { title: 'Gender', field: 'gender', lookup: { M: 'Male', F: 'Female' } },
     { title: 'City', field: 'city' },
+    { title: 'School Fee', field: 'fee', type: 'currency' },
   ];
 
   return (
